2defbaf0ca017aa65b0061909a81df2bd4349862
Nguyen Trieu Huy <nguyentrieuhuy22@gmail.com>
2025-10-31 22:44:46 +0700

    Fix warning payment

--- src/main/java/vn/techbox/techbox_store/payment/model/VnpayPayment.java
+++ src/main/java/vn/techbox/techbox_store/payment/model/VnpayPayment.java
@@ -6,11 +6,13 @@ import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "vnpay_payments")
 @PrimaryKeyJoinColumn(name = "payment_id")
