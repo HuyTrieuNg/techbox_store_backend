6ffe3736ebf9b02b39b29db209ac5907148352eb
ChinhDe <chinhde29@gmail.com>
2025-11-27 16:45:31 +0000

    add get get recently spus for personalized recommeder system

--- src/main/java/vn/techbox/techbox_store/order/service/OrderService.java
+++ src/main/java/vn/techbox/techbox_store/order/service/OrderService.java
@@ -6,6 +6,8 @@ import vn.techbox.techbox_store.order.model.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface OrderService {
 
     OrderResponse createOrder(CreateOrderRequest request, Integer userId);
@@ -19,4 +21,5 @@ public interface OrderService {
     Page<OrderResponse> getOrdersByUserIdAndStatus(Integer userId, OrderStatus status, Pageable pageable);
     OrderResponse updateOrderStatus(Long orderId, OrderStatus status);
     OrderResponse cancelOrder(Long orderId, Integer userId);
+    List<String> getRecentProductSpus(Integer userId, int k);
 }
